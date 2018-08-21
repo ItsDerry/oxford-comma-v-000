b9ec3356d42944816e7384f8b1fcc13255a0e8e7
Derrick Reyes <derrickreyes@me.com>
2018-08-21 21:28:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     else
       new_string = String.new
       array.each do |string|
-      new_string = array[0..-2].join(", ") + " and " + array[-1]
+      new_string = array[0..-2].join(", ") + ", and " + array[-1]
     end
       new_string
   end
